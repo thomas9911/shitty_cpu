d85061683e7e896f270415e658a8481e3cb63b10
Thomas Timmer <thomastimmer11@hotmail.com>
2024-07-07 11:48:05 +0200

    feat: update cli to be able to return custom status code

--- crates/shitty_cli/src/main.rs
+++ crates/shitty_cli/src/main.rs
@@ -2,10 +2,11 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use pico_args::Arguments;
 use shitty_file_format::FileStructure;
 use shitty_parser;
+use std::process::ExitCode;
 
 const HELP_MESSAGE: &str = r#"
     Usage: shitty_cli <subcommand>
@@ -15,11 +16,16 @@ const HELP_MESSAGE: &str = r#"
     run [options] <program>
         options:
             -o, --open <file>
+            --output-as-status-code : return the output as statuscode
+
     compile <input_file> <output_file>
-    exec <file>
+    
+    exec [options] <file>
+        options:
+            --output-as-status-code : return the output as statuscode
 "#;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> Result<ExitCode, anyhow::Error> {
     let mut args = Arguments::from_env();
 
     if args.contains(["-h", "--help"]) {
@@ -27,25 +33,24 @@ fn main() -> Result<(), anyhow::Error> {
     }
 
     match args.subcommand() {
-        Ok(Some(x)) if x == "run" => run(&mut args)?,
-        Ok(Some(x)) if x == "compile" => compile(&mut args)?,
-        Ok(Some(x)) if x == "exec" => exec(&mut args)?,
-        Ok(Some(x)) if x == "help" => help()?,
+        Ok(Some(x)) if x == "run" => run(&mut args),
+        Ok(Some(x)) if x == "compile" => compile(&mut args),
+        Ok(Some(x)) if x == "exec" => exec(&mut args),
+        Ok(Some(x)) if x == "help" => help(),
         _ => {
             anyhow::bail!("Invalid command")
         }
-    };
-
-    Ok(())
+    }
 }
 
-fn help() -> Result<(), anyhow::Error> {
+fn help() -> Result<ExitCode, anyhow::Error> {
     eprintln!("{}", HELP_MESSAGE);
-    Ok(())
+    Ok(ExitCode::SUCCESS)
 }
 
-fn run(args: &mut Arguments) -> Result<(), anyhow::Error> {
-    let file: Option<PathBuf> = args.opt_value_from_str(["-o", "--output"])?;
+fn run(args: &mut Arguments) -> Result<ExitCode, anyhow::Error> {
+    let file: Option<PathBuf> = args.opt_value_from_str(["-o", "--open"])?;
+    let output_as_status_code = args.contains("--output-as-status-code");
     let program_text: Option<String> = args.opt_free_from_str()?;
 
     let program = match (file, program_text) {
@@ -63,12 +68,17 @@ fn run(args: &mut Arguments) -> Result<(), anyhow::Error> {
 
     let mut rt = shitty_runtime::Runtime::new(program);
     rt.run().map_err(|e| anyhow::anyhow!("{}", e))?;
+
+    if output_as_status_code {
+        let status: u8 = rt.output().try_into().context("parsing status code")?;
+        return Ok(ExitCode::from(status));
+    }
     println!("{}", rt.output());
 
-    Ok(())
+    Ok(ExitCode::SUCCESS)
 }
 
-fn compile(args: &mut Arguments) -> Result<(), anyhow::Error> {
+fn compile(args: &mut Arguments) -> Result<ExitCode, anyhow::Error> {
     let input_path: PathBuf = args.free_from_str()?;
     let output_path: PathBuf = args.free_from_str()?;
 
@@ -80,17 +90,22 @@ fn compile(args: &mut Arguments) -> Result<(), anyhow::Error> {
     file.to_path(output_path)
         .map_err(|e| anyhow::anyhow!("{}", e))?;
 
-    Ok(())
+    Ok(ExitCode::SUCCESS)
 }
 
-fn exec(args: &mut Arguments) -> Result<(), anyhow::Error> {
+fn exec(args: &mut Arguments) -> Result<ExitCode, anyhow::Error> {
+    let output_as_status_code = args.contains("--output-as-status-code");
     let file_path: PathBuf = args.free_from_str()?;
 
     let file = FileStructure::from_path(file_path).map_err(|e| anyhow::anyhow!("{}", e))?;
     let mut rt = shitty_runtime::Runtime::new(file.program);
     rt.run().map_err(|e| anyhow::anyhow!("{}", e))?;
+    if output_as_status_code {
+        let status: u8 = rt.output().try_into().context("parsing status code")?;
+        return Ok(ExitCode::from(status));
+    }
     println!("{}", rt.output());
-    Ok(())
+    Ok(ExitCode::SUCCESS)
 }
 
 #[test]
@@ -115,6 +130,6 @@ fn run_from_file() {
 
     let mut args = Arguments::from_vec(vec!["-o".into(), OsString::from(&file_path)]);
     assert!(run(&mut args).is_ok());
-    let mut args = Arguments::from_vec(vec!["--output".into(), OsString::from(file_path)]);
+    let mut args = Arguments::from_vec(vec!["--open".into(), OsString::from(file_path)]);
     assert!(run(&mut args).is_ok());
 }
